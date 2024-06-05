cb949266dfcf5d8c0e003af7f81dfb0552f5c561
Mad Martigane <martigane.mad@gmail.com>
2024-06-05 07:45:16 +0200

    Remove vite-plugins-copy used for shoelace

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,9 @@ import solidPlugin from 'vite-plugin-solid'
 import suidPlugin from '@suid/vite-plugin'
 
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/@shoelace-style/shoelace/dist/assets/icons/*.svg',
-          dest: 'assets/icons',
-        },
-      ],
-    }),
-    suidPlugin(),
-  ],
+  plugins: [solidPlugin(), suidPlugin()],
   server: {
     port: 3000,
   },
