8469fffc6a0f021665a1ef974d533230f179ed89
Mad Martigane <martigane.mad@gmail.com>
2024-09-08 16:12:47 +0200

    fix(stat-rebond-sum-up): fix display on mobile device

--- src/components/match/match.tsx
+++ src/components/match/match.tsx
@@ -246,29 +246,31 @@ function renderStatGrid(statSummary: StatMatchSummary) {
       <hr />
 
       <h3>Synthèse rebonds</h3>
-      <div class="stats shadow">
-        <div class="stat place-items-center">
-          <div class="stat-title">Total</div>
-          <div
-            class={`stat-value ${statSummary.rebonds.teamTotalPercentage > 49 ? 'text-success' : 'text-warning'}`}
-          >{`${statSummary.rebonds.teamTotalPercentage} %`}</div>
-          <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamTotal}) - Opposent (${statSummary.rebonds.opponentTotal})`}</div>
-        </div>
+      <div class="overflow-x-auto">
+        <div class="stats shadow">
+          <div class="stat place-items-center">
+            <div class="stat-title">Total</div>
+            <div
+              class={`stat-value ${statSummary.rebonds.teamTotalPercentage > 49 ? 'text-success' : 'text-warning'}`}
+            >{`${statSummary.rebonds.teamTotalPercentage} %`}</div>
+            <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamTotal}) - Opposent (${statSummary.rebonds.opponentTotal})`}</div>
+          </div>
 
-        <div class="stat place-items-center">
-          <div class="stat-title">Offensifs</div>
-          <div
-            class={`stat-value ${statSummary.rebonds.teamOffensivePercentage > 49 ? 'text-success' : 'text-warning'}`}
-          >{`${statSummary.rebonds.teamOffensivePercentage} %`}</div>
-          <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamOffensive}) - Opposent (${statSummary.rebonds.opponentOffensive})`}</div>
-        </div>
+          <div class="stat place-items-center">
+            <div class="stat-title">Offensifs</div>
+            <div
+              class={`stat-value ${statSummary.rebonds.teamOffensivePercentage > 49 ? 'text-success' : 'text-warning'}`}
+            >{`${statSummary.rebonds.teamOffensivePercentage} %`}</div>
+            <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamOffensive}) - Opposent (${statSummary.rebonds.opponentOffensive})`}</div>
+          </div>
 
-        <div class="stat place-items-center">
-          <div class="stat-title">Defensifs</div>
-          <div
-            class={`stat-value ${statSummary.rebonds.teamDefensivePercentage > 49 ? 'text-success' : 'text-warning'}`}
-          >{`${statSummary.rebonds.teamDefensivePercentage} %`}</div>
-          <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamDefensive}) - Opposent (${statSummary.rebonds.opponentDefensive})`}</div>
+          <div class="stat place-items-center">
+            <div class="stat-title">Defensifs</div>
+            <div
+              class={`stat-value ${statSummary.rebonds.teamDefensivePercentage > 49 ? 'text-success' : 'text-warning'}`}
+            >{`${statSummary.rebonds.teamDefensivePercentage} %`}</div>
+            <div class="stat-desc">{`Équipe (${statSummary.rebonds.teamDefensive}) - Opposent (${statSummary.rebonds.opponentDefensive})`}</div>
+          </div>
         </div>
       </div>
     </div>
